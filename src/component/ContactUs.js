96b91b4e550c4ff158701e08778598094fc50f90
ranjan_4590 <ranjangupta35558@gmail.com>
2023-07-11 23:48:50 +0530

    title

--- src/component/ContactUs.js
+++ src/component/ContactUs.js
@@ -38,7 +38,7 @@ function ContactUs() {
     `;
     return (
         <Wrapper>
-            <iframe  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3431.1336476055835!2d76.66011161497433!3d30.68651558165328!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390fef5f2758e69f%3A0xd260ad255a8f0454!2sCgc%20Landran%20Volleyball%20Court!5e0!3m2!1sen!2sin!4v1660142840997!5m2!1sen!2sin" 
+            <iframe title="This is a unique title" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3431.1336476055835!2d76.66011161497433!3d30.68651558165328!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390fef5f2758e69f%3A0xd260ad255a8f0454!2sCgc%20Landran%20Volleyball%20Court!5e0!3m2!1sen!2sin!4v1660142840997!5m2!1sen!2sin" 
             width="100%" height="450" style={{border:0}} allowfullscreen="" loading="lazy"
             referrerpolicy="no-referrer-when-downgrade">
             </iframe>
